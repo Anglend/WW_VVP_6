03412af446c416019cdecf90825b2b634147c9db
Semackov Maxim <MyEmail@MYEMAIL.RU>
2026-04-21 12:04:36 +0300

    add parsing JSON

--- Service.h
+++ Service.h
@@ -5,3 +5,10 @@ class Service
 	virtual Weather getWeather(std::string s) = 0;
 	virtual ~Service();
 };
+
+class JsonService :public Service
+{
+public:
+	virtual Weather getWeather(std::string s) override;
+	virtual ~JsonService() {};
+};
