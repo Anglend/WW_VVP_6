1819d2f1ff594daffbe84161e4a734830c5ae2e1
Semackov Maxim <MyEmail@MYEMAIL.RU>
2026-04-21 10:39:41 +0300

    add main and class Weather

--- Weather.h
+++ Weather.h
@@ -1,5 +1,17 @@
 #pragma once
+#include <string>
 class Weather
 {
+private:
+	std::string city; //  Киров
+	double lon; // 49.6601
+	double lat; // 58.5966
+	double temperature; // 5.69
+	std::string weather; // дождь
+	double windSpeed; // 4.27
+	int clouds; // 100
+public:
+	Weather(std::string, double, double, double, std::string, double, int);
+
 };
 
