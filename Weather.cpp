acd77d85cb708ea5c2bd5e17ceac06718f2ac460
Semackov Maxim <MyEmail@MYEMAIL.RU>
2026-04-21 10:49:07 +0300

    add description constructor

--- Weather.cpp
+++ Weather.cpp
@@ -1,1 +1,5 @@
 #include "Weather.h"
+
+
+
+Weather::Weather(std::string city1, double lon1, double lat1, double temperature1, std::string weather1, double windSpeed1, int clouds1) : city(city1), lon(lon1), lat(lat1), temperature(temperature1), weather(weather1), windSpeed(windSpeed1), clouds(clouds1)	{ }
